a862a0efbc35fdf096f416a511c4fde70cdf30a7
Jafet Zagal <zagalids@gmail.com>
2025-01-25 15:25:25 -0600

    adding CRUD operations

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+class Project(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+    ]
+
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    location = models.CharField(max_length=255)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Project
+from .serializers import ProjectSerializer
 
 # Create your views here.
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
