bb9b0accd8dc9b28e73cb9873f9b76b29b95a8ce
Jafet Zagal <zagalids@gmail.com>
2025-01-25 23:01:35 -0600

    adding google maps api validation

--- oowlish/settings.py
+++ oowlish/settings.py
@@ -127,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GOOGLE_MAPS_API_KEY = 'AIzaSyCoJ4WPKpMgm4lMFUkvro6AhrL3odIvwPA'
--- projects/serializers.py
+++ projects/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
 from .models import Project
+import googlemaps
+from django.conf import settings
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = '__all__'
+
+    def validate(self, data):
+        if not data.get('latitude') or not data.get('longitude'):
+            gmaps = googlemaps.Client(key=settings.GOOGLE_MAPS_API_KEY)
+            geocode_result = gmaps.geocode(data['location'])
+            if geocode_result:
+                location = geocode_result[0]['geometry']['location']
+                data['latitude'] = location['lat']
+                data['longitude'] = location['lng']
+            else:
+                raise serializers.ValidationError("No se pudo validar la dirección proporcionada.")
+        return data
