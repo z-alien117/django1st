0b7460df47200f0f44090f974ec3b27e787e62c0
Jafet Zagal <zagalids@gmail.com>
2025-01-27 10:42:43 -0600

    changing documentation

--- projects/views.py
+++ projects/views.py
@@ -11,9 +11,7 @@ from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from django.http import HttpResponse
 
-
-# Create your views here.
-
+# API ViewSet for Project model
 class ProjectViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows projects to be viewed or edited.
@@ -26,6 +24,9 @@ class ProjectViewSet(viewsets.ModelViewSet):
         responses={200: ProjectSerializer(many=True)}
     )
     def list(self, request, *args, **kwargs):
+        """
+        List all projects.
+        """
         return super().list(request, *args, **kwargs)
 
     @swagger_auto_schema(
@@ -34,6 +35,9 @@ class ProjectViewSet(viewsets.ModelViewSet):
         responses={201: ProjectSerializer()}
     )
     def create(self, request, *args, **kwargs):
+        """
+        Create a new project.
+        """
         return super().create(request, *args, **kwargs)
 
     @swagger_auto_schema(
@@ -41,27 +45,41 @@ class ProjectViewSet(viewsets.ModelViewSet):
         responses={200: ProjectSerializer()}
     )
     def retrieve(self, request, *args, **kwargs):
+        """
+        Retrieve a specific project by ID.
+        """
         return super().retrieve(request, *args, **kwargs)
 
-
-# Vistas basadas en clases para el CRUD
+# Class-based views for CRUD operations on Project model
 class ProjectListView(ListView):
+    """
+    View to list all projects.
+    """
     model = Project
-    template_name = 'projects/project_list.html'  # Ruta a la plantilla para listar proyectos
-    context_object_name = 'projects'  # Nombre de la variable en el contexto
+    template_name = 'projects/project_list.html'
+    context_object_name = 'projects'
 
 class ProjectDetailView(DetailView):
+    """
+    View to display details of a specific project.
+    """
     model = Project
-    template_name = 'projects/project_detail.html'  # Ruta a la plantilla para detalles de un proyecto
-    context_object_name = 'project'  # Nombre de la variable en el contexto
+    template_name = 'projects/project_detail.html'
+    context_object_name = 'project'
 
 class ProjectCreateView(CreateView):
+    """
+    View to create a new project.
+    """
     model = Project
-    form_class = ProjectForm  # Usar el formulario personalizado para crear un proyecto
-    template_name = 'projects/project_form.html'  # Ruta a la plantilla para crear un proyecto
-    success_url = reverse_lazy('project_list')  # Redirección al listar proyectos tras crear uno
+    form_class = ProjectForm
+    template_name = 'projects/project_form.html'
+    success_url = reverse_lazy('project_list')
 
     def form_valid(self, form):
+        """
+        Validate and save the form.
+        """
         serializer = ProjectSerializer(data=form.cleaned_data)
         if not serializer.is_valid():
             for field, errors in serializer.errors.items():
@@ -69,19 +87,24 @@ class ProjectCreateView(CreateView):
                     form.add_error(field, error)
             return self.form_invalid(form)
 
-        # Asignar latitud y longitud desde el serializer
         form.instance.latitude = serializer.validated_data.get('latitude')
         form.instance.longitude = serializer.validated_data.get('longitude')
 
         return super().form_valid(form)
 
 class ProjectUpdateView(UpdateView):
+    """
+    View to update an existing project.
+    """
     model = Project
-    form_class = ProjectForm  # Usar el formulario personalizado para crear un proyecto
-    template_name = 'projects/project_form.html'  # Reutilizamos la plantilla para crear/editar
-    success_url = reverse_lazy('project_list')  # Redirección al listar proyectos tras editar uno
+    form_class = ProjectForm
+    template_name = 'projects/project_form.html'
+    success_url = reverse_lazy('project_list')
 
     def form_valid(self, form):
+        """
+        Validate and save the form.
+        """
         serializer = ProjectSerializer(instance=self.object, data=form.cleaned_data)
         if not serializer.is_valid():
             for field, errors in serializer.errors.items():
@@ -89,23 +112,31 @@ class ProjectUpdateView(UpdateView):
                     form.add_error(field, error)
             return self.form_invalid(form)
 
-        # Asignar latitud y longitud desde el serializer
         form.instance.latitude = serializer.validated_data.get('latitude')
         form.instance.longitude = serializer.validated_data.get('longitude')
 
         return super().form_valid(form)
 
 class ProjectDeleteView(DeleteView):
+    """
+    View to delete a project.
+    """
     model = Project
-    template_name = 'projects/project_confirm_delete.html'  # Ruta a la plantilla para confirmar eliminación
-    success_url = reverse_lazy('project_list')  # Redirección al listar proyectos tras eliminar uno
+    template_name = 'projects/project_confirm_delete.html'
+    success_url = reverse_lazy('project_list')
 
 class ProjectDistanceListView(ListView):
+    """
+    View to list projects along with the distances between them.
+    """
     model = Project
     template_name = 'projects/project_distance_list.html'
     context_object_name = 'projects'
 
     def get_context_data(self, **kwargs):
+        """
+        Add distances between projects to the context.
+        """
         context = super().get_context_data(**kwargs)
         projects = context['projects']
         distances = []
@@ -125,6 +156,9 @@ class ProjectDistanceListView(ListView):
 
         context['distances'] = distances
         return context
-    
+
 def health_check(request):
+    """
+    Health check endpoint.
+    """
     return HttpResponse(status=200)
